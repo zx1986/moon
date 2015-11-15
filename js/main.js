fc49d7a176a1da0169a41ec7787c7078b814de99
Sunny <gsunny4178@gmail.com>
2015-11-15 10:52:50 +0800

    test

--- js/main.js
+++ js/main.js
@@ -140,6 +140,7 @@ d3.json('data/moon.json', function (err, data) {
     // your AJAX to check if it avaiable
     // ....
     console.log('status: ', id);
+    console.log('Test: ', id);
     return true;
   }
 
